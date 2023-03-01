2929c705936b064c20f72061cfc329596bea24fe
hoxyshmoo <52104528+hoxyshmoo@users.noreply.github.com>
2023-03-01 19:36:13 +0000

    Nav MEsh Done

--- GameDevelopmentCw1/Assets/Scripts/CharacterScript/FollowPlayer.cs
+++ GameDevelopmentCw1/Assets/Scripts/CharacterScript/FollowPlayer.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform target;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    private void Awake(){
+        agent=GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation=false;
+        agent.updateUpAxis=false;
+
+    }
 
     private void Start(){
-        target=GameObject.FindWithTag("Player").transform;
+        //target=GameObject.FindWithTag("Player").transform;
+      target=GameObject.FindWithTag("Player").transform;
+    }
 
+    private void setAgentPosition(){
+        agent.SetDestination(new Vector3(target.transform.position.x,target.transform.position.y,transform.position.z));
     }
 
     private void Update(){
         if(Vector2.Distance(transform.position,target.position)> 2){
-            transform.position=Vector2.MoveTowards(transform.position,target.position,3*Time.deltaTime);
+          //  transform.position=Vector2.MoveTowards(transform.position,target.position,3*Time.deltaTime);
+                 setAgentPosition();
         }
+     
+      
     }
+
+
  
 }
